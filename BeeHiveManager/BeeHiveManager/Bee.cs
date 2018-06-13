0c5299453513662485fcc1f95c6cfa2c097507d7
Rick van Dijk <rickavdijk@gmail.com>
2018-06-13 16:51:49 +0200

    First exercise with Interfaces

---
+++ BeeHiveManager/BeeHiveManager/Bee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHiveManager
+{
+    class Bee : IStingPatrol
+    {
+        public int AlertLevel { get; }
+
+        public int StingerLength { get; set; }
+
+        public bool LookForEnemies()
+        {
+            return true;
+        }
+
+        public int SharpenStinger(int length)
+        {
+            return 5;
+        }
+    }
+}
---
+++ BeeHiveManager/BeeHiveManager/IStingPatrol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHiveManager
+{
+    interface IStingPatrol
+    {
+        int AlertLevel { get; }
+        int StingerLength { get; set; }
+        bool LookForEnemies();
+        int SharpenStinger(int length);
+    }
+    interface INectarCollector
+    {
+        void FindFlowers();
+        void GatherNectar();
+        void ReturnToHive();
+    }
+}
